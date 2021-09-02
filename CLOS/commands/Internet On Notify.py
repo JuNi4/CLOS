4bdfba68ab0cdd8118b5d730d21a9178c717f7d1
JuNi4 <justus@hrtm.name>
2021-09-02 19:33:16 +0200

    Create Internet On Notify.py

---
+++ CLOS/commands/Internet On Notify.py
@@ -0,0 +1,44 @@
+import http.client as httplib
+from datetime import datetime
+from pythonping import ping
+import time
+import os
+
+alert = 'D:\Justus\Videos\\4K Video Downloader\karlson vibe [epilepsy warning] (perfect loop).mp4'
+
+def internet():
+    conn = httplib.HTTPConnection("www.google.com", timeout=5)
+    try:
+        conn.request("HEAD", "/")
+        conn.close()
+        return True
+    except:
+        conn.close()
+        return False
+
+x = 1
+x2 = 0
+alert = '"'+alert+'"'
+
+while True:
+    if internet() == True:
+        response_list = ping('google.com', size=40, count=4)
+        ms = str(response_list.rtt_avg_ms)
+        print('Currently Your Connected To The Internet With A Ping Of '+ms+'ms.                   ', end='\r')
+        x = 1
+        if x2 == 1:
+            os.system(alert)
+        x2 = 0
+    else:
+        if x == 1:
+            time_time = datetime.now()
+            start_time = time_time.strftime("%Hh %Mm %Ss")
+            starttime = time.time()
+        x = 0
+        x2 = 1
+        endtime = time.time()
+        inp = endtime - starttime
+        hui = int(inp/60)
+        mrc = hui*60
+        mrf = inp - mrc
+        print('Currently There Is No Connection With The Internet Since '+str(start_time)+' for '+str(hui)+'m '+str(mrf)[:2]+'s.         ',end='\r')
