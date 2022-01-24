c4c8d8166899cc4ec48ea4e03d21ac1aa939f8d4
JuNi4 <justus@hrtm.name>
2022-01-24 13:02:43 +0100

    Update color.py

--- CLOS/commands/color.py
+++ CLOS/commands/color.py
@@ -1,7 +1,7 @@
 # Color command from Windows CMD:
 import sys
 arg = sys.argv
-helptxt = 'Legt die standardmäßigen Hinter- und Vordergrundfarben für die Konsole fest.\n\nCOLOR [attr]\n\n  attr      Gibt die Farbattribute für die Konsolenausgabe an.\n\nFarbattribute werden durch ZWEI hexadezimale Zeichen angegeben – das erste\nbezieht sich auf den Hintergrund, das zweite auf den Vordergrund. Jedes Zeichen\nkann einen der folgenden Werte annehmen:\n\n    0 = Schwarz       8 = Grau\n    1 = Blau        9 = Hellblau\n    2 = Grün       A = Hellgrün\n    3 = Türkis        B = Helltürkis\n    4 = Rot         C = Hellrot\n    5 = Lila      D = Helllila\n    6 = Gelb      E = Hellgelb\n    7 = Hellgrau       F = Weiß\n\nWenn der Befehl ohne Argument aufgerufen wird, werden die Farbein-\nstellungen wiederhergestellt, mit denen CMD.EXE gestartet wurde. Diese werden\ndurch das aktuelle Konsolenfenster, die /T-Befehlszeilenoption oder durch den\nRegistrierungswert "DefaultColor" bestimmt.\n\nDer COLOR-Befehl legt ERRORLEVEL auf 1 fest, wenn versucht wird,\ndiesen Befehl mit einer Vordergrundfarbe auszuführen, die mit der Hinter-\ngrundfarbe identisch ist.\n\nBeispiel: "COLOR fc" generiert Hellrot auf weißem Hintergrund.'
+helptxt = 'Legt die standardmäßigen Hinter- und Vordergrundfarben für die Konsole fest.\n\nCOLOR [attr]\n\n  attr      Gibt die Farbattribute für die Konsolenausgabe an.\n\nFarbattribute werden durch ZWEI hexadezimale Zeichen angegeben – das erste\nbezieht sich auf den Hintergrund, das zweite auf den Vordergrund. Jedes Zeichen\nkann einen der folgenden Werte annehmen:\n\n    0 = Schwarz   8 = Grau\n    1 = Blau      9 = Hellblau\n    2 = Grün      A = Hellgrün\n    3 = Türkis    B = Helltürkis\n    4 = Rot       C = Hellrot\n    5 = Lila      D = Helllila\n    6 = Gelb      E = Hellgelb\n    7 = Hellgrau  F = Weiß\n                  R = Reset\n\nWenn der Befehl ohne Argument aufgerufen wird, werden die Farbein-\nstellungen wiederhergestellt, mit denen CMD.EXE gestartet wurde. Diese werden\ndurch das aktuelle Konsolenfenster, die /T-Befehlszeilenoption oder durch den\nRegistrierungswert "DefaultColor" bestimmt.\n\nDer COLOR-Befehl legt ERRORLEVEL auf 1 fest, wenn versucht wird,\ndiesen Befehl mit einer Vordergrundfarbe auszuführen, die mit der Hinter-\ngrundfarbe identisch ist.\n\nBeispiel: "COLOR fc" generiert Hellrot auf weißem Hintergrund.'
 
 class color:
     Default      = '\033[39m'
@@ -41,15 +41,18 @@ class bcolor:
     LightCyan    = '\033[106m'
     White        = '\033[107m'
 
-colors = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-corospondst = [color.Black,color.Blue,color.Green,color.Cyan,color.Red,color.Magenta,color.Yellow,color.LightGray,color.DarkGray,color.LightBlue,color.LightGreen,color.LightCyan,color.LightRed,color.LightMagenta,color.LightYellow]
-corospondsb = [bcolor.Black,bcolor.Blue,bcolor.Green,bcolor.Cyan,bcolor.Red,bcolor.Magenta,bcolor.Yellow,bcolor.LightGray,bcolor.DarkGray,bcolor.LightBlue,bcolor.LightGreen,bcolor.LightCyan,bcolor.LightRed,bcolor.LightMagenta,bcolor.LightYellow]
+colors = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','R']
+corospondst = [color.Black,color.Blue,color.Green,color.Cyan,color.Red,color.Magenta,color.Yellow,color.LightGray,color.DarkGray,color.LightBlue,color.LightGreen,color.LightCyan,color.LightRed,color.LightMagenta,color.LightYellow,color.White,color.Default]
+corospondsb = [bcolor.Black,bcolor.Blue,bcolor.Green,bcolor.Cyan,bcolor.Red,bcolor.Magenta,bcolor.Yellow,bcolor.LightGray,bcolor.DarkGray,bcolor.LightBlue,bcolor.LightGreen,bcolor.LightCyan,bcolor.LightRed,bcolor.LightMagenta,bcolor.LightYellow,bcolor.White,bcolor.Default]
 if len(arg) == 1 or sys.argv[1] == '/?':
     print(helptxt)
     exit()
 else:
-    if len(arg[1]) == 2:        
-        print(corospondst[colors.index(arg[1][1:2].upper())])
-        print(corospondsb[colors.index(arg[1][:1].upper())])
+    if arg[1] == 'reset':
+        print(color.Default+bcolor.Default, end = "")
+        exit()
+    if len(arg[1]) == 2:
+        print(corospondst[colors.index(arg[1][1:2].upper())], end = "")
+        print(corospondsb[colors.index(arg[1][:1].upper())], end = "")
     else:
-        print(corospondst[colors.index(arg[1][:1].upper())])
+        print(corospondst[colors.index(arg[1][:1].upper())], end = "")
